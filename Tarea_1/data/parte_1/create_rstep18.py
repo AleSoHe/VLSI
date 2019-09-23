d8a6d4d12e7437b41832059f57ef4cb067f598f7
AleSoHe <alejandrosohe@hotmail.com>
2019-09-22 19:06:00 -0600

    parte 1 y dos avance

--- Tarea_1/data/parte_1/create_rstep18.py
+++ Tarea_1/data/parte_1/create_rstep18.py
@@ -1,8 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-filename = 'parteA_NMOS_rstep'
-
 class system():
 	def __init__(self, name, filepath):
 		self.name = name
@@ -25,11 +23,11 @@ class measure():
 		self.scale = scale
 		self.system = _system
 		self.data = []
-		self.time = None
+		self.x = None
 		
 	def plot_measure(self):
-		plt.plot(np.array(self.time.data)*self.time.scale, np.array(self.data)*self.scale)
-		plt.xlabel(self.time.label)
+		plt.plot(np.array(self.x.data), np.array(self.data)*self.scale)
+		plt.xlabel(self.x.label)
 		plt.ylabel(self.label)
 		g1 = plt.grid(b=True, which='major', color='k', linestyle='-', linewidth=0.2)
 		#g2 = plt.grid(b=True, which='minor', color='k', linestyle='-', linewidth=0.05)
@@ -42,14 +40,49 @@ class measure():
 			_measure.plot_measure()
 		plt.suptitle(measure_list[0].system)
 		plt.show()
-			
-# System 
-system_name = 'NMOS_Rstep'		
+	
+# --------
+# --- PMOS
+# --------
+# DATA filename
+filename = 'parteA_PMOS_rstep'
+system_name = 'RSTEP'
+
 
 # Measures
 time = measure('Time', 'time [ns]', 1000000000, system_name)
 Idrain = measure('Drain current', 'Id [uA]', 1000000, system_name)
 Vdrain = measure('Drain Voltage', 'Vd [V]', 1, system_name)
+
+
+# Read file
+with open(filename, 'r') as f:
+	lines = f.readlines()[2:]
+	for line in lines:
+		words = line.split()
+		time.data.append(float(words[0]))
+		Idrain.data.append(float(words[1])*Idrain.scale)
+		Vdrain.data.append(float(words[2])-1.79)
+	f.close()
+
+
+filename = 'rstep_P18'
+with open(filename, 'w+') as f:
+	f.write('Vds Id_uA\n')
+	for v,i in zip(Vdrain.data[0::3], Idrain.data[0::3]):
+		f.write(str(v) + ' ' + str(i) + '\n')
+	f.close()
+
+# --------
+# --- NMOS
+# --------			
+# DATA filename
+filename = 'parteA_NMOS_rstep'
+
+time.data = []
+Idrain.data = []
+Vdrain.data = []
+
 
 # Read file
 with open(filename, 'r') as f:
@@ -58,26 +91,15 @@ with open(filename, 'r') as f:
 		words = line.split()
 		print(words)
 		time.data.append(float(words[0]))
-		Idrain.data.append(float(words[1]))
+		Idrain.data.append(float(words[1])*Idrain.scale)
 		Vdrain.data.append(float(words[2]))
 	f.close()
-	
-# Assign time vectors
-Idrain.time = time
-Vdrain.time = time
-del time
 
-# PLOT DATA
-
-plt.subplot(2, 1, 1)
-Idrain.plot_measure()
-plt.annotate('(114 nA)', xy=(7.69,114), xytext=(12.69,114),
-            arrowprops=dict(arrowstyle='->'))
-
-plt.subplot(2, 1, 2)
-Vdrain.plot_measure()
-plt.annotate('0.9 V', xy=(7.69,0.9), xytext=(12.69,0.9),
-            arrowprops=dict(arrowstyle='->'))
-plt.show()
-
-#measure.subplot_measures([Idrain,Vdrain])
+filename = 'rstep_N18'
+with open(filename, 'w+') as f:
+	f.write('Vds Id_uA\n')
+	for v,i in zip(Vdrain.data[0::3], Idrain.data[0::3]):
+		f.write(str(v) + ' ' + str(i) + '\n')
+	f.close()
+		
+# Assign X vectors
